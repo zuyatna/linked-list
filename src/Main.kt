36a482d527c1b389b7ad7afb8154824912fa47d0
Suyatna <zuyatna@gmail.com>
2024-07-12 18:26:24 +0700

    Nodes next

--- src/Main.kt
+++ src/Main.kt
@@ -12,6 +12,14 @@ fun createListWithValues(size: Int): Node<Int>? {
 }
 
 fun main() {
-    val newList = createListWithValues(5)
-    println(newList)
+//    val newList = createListWithValues(5)
+//    println(newList)
+    val nodes1 = Node(value = 1)
+    val nodes2 = Node(value = 2)
+    val nodes3 = Node(value = 3)
+
+    nodes1.next = nodes2
+    nodes2.next = nodes3
+
+    println(nodes1)
 }
